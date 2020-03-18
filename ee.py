1b220c45d1f8b611b4a3bfb3f3b3522054b7649f
DSLYL <1150475496@qq.com>
2020-03-19 01:19:59 +0800

    第二次上传

--- ee.py
+++ ee.py
@@ -1,3 +1,18 @@
-def mm():
-    print(2)
-mm()
+import random
+
+ss1 = 2
+print(ss1)
+for i in range(100):
+    print(random.randint(1, 10))
+
+
+def main():
+    number = 4
+    if number > 2:
+        print("哈哈。答对了")
+    else:
+        print("哈哈。答错了")
+
+
+if __name__ == "__main__":
+    main()
